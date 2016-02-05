db3f8a90d14a9dd252967adfff0fbe5d8ba827b8
Ashley Kleynhans <ashley@digigoblin.com>
2016-02-05 03:13:53 +0200

    Started implementing the frontend

--- app/public/assets/js/forex.js
+++ app/public/assets/js/forex.js
@@ -2,8 +2,24 @@ $(document).ready(function() {
     var jqxhr = $.get("http://127.0.0.1:9090/v1/currency", function() {
 
     })
-        .done(function(data) {
-            alert( "Data Loaded: " + data );
+        .done(function(response) {
+            if (response.status == 'success') {
+                var row = '<div class="row title">' +
+                    '<div class="col-xs-6 col-md-4">Currency</div>' +
+                    '<div class="col-xs-6 col-md-2">Exchange Rate</div>' +
+                    '<div class="col-xs-6 col-md-2">Surcharge</div>' +
+                    '</div>';
+                $('#currencies').append(row);
+
+                $.each(response.data, function (item, data) {
+                    var row = '<div class="row">' +
+                        '<div class="col-xs-6 col-md-4">'+ data.currency_name +' ('+ data.currency_code +')</div>' +
+                        '<div class="col-xs-6 col-md-2">'+ data.exchange_rate + '</div>' +
+                        '<div class="col-xs-6 col-md-2">'+ data.currency_surcharge + '</div>' +
+                        '</div>';
+                    $('#currencies').append(row);
+                })
+            }
         })
         .fail(function() {
             alert("Failed to load currency data");
